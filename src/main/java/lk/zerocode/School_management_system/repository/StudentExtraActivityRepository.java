aa148640e992778dee453b51ae05813389315541
thushan99 <m.t.miuranga@gmail.com>
2024-12-06 12:55:26 +0530

    unnecessary file delete

--- src/main/java/lk/zerocode/School_management_system/repository/StudentExtraActivityRepository.java
+++
@@ -1,7 +0,0 @@
-package lk.zerocode.School_management_system.repository;
-
-import lk.zerocode.School_management_system.model.StudentExtraActivity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentExtraActivityRepository extends JpaRepository<StudentExtraActivity, Long> {
-}
